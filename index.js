06c9ddcbd82e2e0931109b9fd68263fa994285ea
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-16 11:59:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+ // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift('Milo');
+}
+
+let newCats = cats.concat(['Broom'])
+
+function appendCat(){
+    return newCats
+}
+
+const copyOfCats = [...cats]
+copyOfCats.unshift("Arnold")
+function prependCat () {
+
+    return copyOfCats
+}
+
+const feline = [...cats]
+function removeLastCat() {
+    console.log(feline.pop())
+    return feline
+}
+
+const kitten = [...cats]
+function removeFirstCat() {
+    console.log(kitten.shift())
+    return kitten
+}
